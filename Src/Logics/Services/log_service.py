32b2a22bc7179cd6a3c86fc66ee47057732054bd
Zelmoron <ak.igor13@mail.ru>
2024-05-12 14:32:26 +0800

    docker

--- Src/Logics/Services/log_service.py
+++ Src/Logics/Services/log_service.py
@@ -34,6 +34,12 @@ class log_service(service):
         exception_proxy.validate(value, error_proxy)
         self.__item = value
 
+    def get_log_record(self, data):
+        factory = convert_factory()
+        data = factory.serialize( data )
+        json_text = json.dumps(data, sort_keys = True, indent = 4, ensure_ascii = False)  
+        return json_text
+
     def __observe_save_log(self):
         """
             Сохранить лог и очистить историю
@@ -47,10 +53,8 @@ class log_service(service):
                 os.remove(log_file)
 
             factory = convert_factory()
-            with open(log_file, "w") as write_file: 
-                data = factory.serialize( data )
-                json_text = json.dumps(data, sort_keys = True, indent = 4, ensure_ascii = False)  
-                write_file.write(json_text)
+            with open(log_file, "w") as write_file:  
+                write_file.write(self.get_log_record(data))
 
         except Exception as ex:
             raise Exception(f"Ошибка при записи файла {log_file}\n{ex}")        
@@ -68,6 +72,7 @@ class log_service(service):
         # Добавить запись в лог
         if handle_type == event_type.write_log() and self.__item is not None:
             self.__data.append(self.__item)
+            print(self.get_log_record(self.__item))
             
         # Записать лог в файл и очистить    
         if handle_type == event_type.save_log():
